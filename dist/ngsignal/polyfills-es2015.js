c7387ea8f8a46cb76793d43a50016a06d7952ead
Saber Sight <thesabersight@gmail.com>
2020-08-16 09:36:39 -0700

    initial layout

--- dist/ngsignal/polyfills-es2015.js
+++ dist/ngsignal/polyfills-es2015.js
@@ -3033,7 +3033,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/ujjwal/signal/client/ngsignal/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /home/ujjwal/signal/ujjwalgeo/signal-client/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
--- dist/ngsignal/polyfills-es5.js
+++ dist/ngsignal/polyfills-es5.js
@@ -19078,7 +19078,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
   /***/
   function _(module, exports, __webpack_require__) {
     __webpack_require__(
-    /*! /home/ujjwal/signal/client/ngsignal/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js */
+    /*! /home/ujjwal/signal/ujjwalgeo/signal-client/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js */
     "./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/es5-polyfills.js");
 
     __webpack_require__(
@@ -19086,7 +19086,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
     "./node_modules/zone.js/dist/zone-legacy.js");
 
     module.exports = __webpack_require__(
-    /*! /home/ujjwal/signal/client/ngsignal/src/polyfills.ts */
+    /*! /home/ujjwal/signal/ujjwalgeo/signal-client/src/polyfills.ts */
     "./src/polyfills.ts");
     /***/
   }
