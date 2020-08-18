32fd72a7838edb6fdfc9995b3d65dc4d151b818b
Saber Sight <thesabersight@gmail.com>
2020-08-18 15:24:25 -0700

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,22 @@
+// The list of file replacements can be found in `angular.json`.
+
+const geonodeHost = 'http://portal.signaltech.us/';
+const clientId = 'wn3BiioINRLbmKs2V4bdCILRb8G0iRJUCo807kjg';
+const redirectUrl = 'http://dashboard.signaltech.us/auth-landing&scope=read%20write%20groups';
+
+
 export const environment = {
-  production: true
+  production: true,
+  geonode: {
+    host: geonodeHost,
+    oauth: {
+      url: geonodeHost + 'o/authorize/?client_id=' + clientId +
+        '&response_type=token&state=random_state_string&' +
+        'redirect_uri=' + redirectUrl,
+      clientId: 'Fv10rMeOGcXMdZubBogL2sP9AyQf6mIN7RARGWhw'
+    }
+  },
+  signalapi: {
+    url: geonodeHost + 'signalapi'
+  }
 };
